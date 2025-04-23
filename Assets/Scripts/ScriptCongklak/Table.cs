2c5986157da83f2cd063da8a1607442e6e65d420
Shalo7 <sheva.070205@gmail.com>
2025-04-23 16:24:17 +0700

    Congklak dan Tali
    masukkin model congklak dan tali, prototype congklak juga udh jadi

--- Assets/Scripts/ScriptCongklak/Table.cs
+++ Assets/Scripts/ScriptCongklak/Table.cs
@@ -12,7 +12,7 @@ public class Table
         for (int i = 0; i < 7; i++)
         {
             _data[i] = 7;
-            _data[i + 8] = 4;
+            _data[i + 8] = 7;
         }
         _currentPlayer = 0;
     }
@@ -29,37 +29,97 @@ public class Table
 
     public bool Move(int index)
     {
-        
+        if (!IsValidMove(index))
+        {
+            return false;
+        }
+        int pickIndex = index + _currentPlayer * 8;
+        int placeCount = _data[pickIndex];
+
+        int tileIndex = -1;
+        _data[pickIndex] = 0;
+        for (int i = 0; i < placeCount; i++)
+        {
+            tileIndex = (pickIndex + i + 1) % 16;
+            if (tileIndex == 7 + (1 - _currentPlayer) * 8)
+            {
+                placeCount++;
+                continue;
+            }
+            _data[tileIndex]++;
+        }
+
+        int currentPlayerScoreTile = GetPlayerScoreTitle(_currentPlayer);
+        if (tileIndex == currentPlayerScoreTile)
+        {
+            if (!HasPlayerValidMove(_currentPlayer))
+            {
+                _currentPlayer = 1 - _currentPlayer;
+            }
+            return true;
+        }
+
+        if (IsField(tileIndex) && _data[tileIndex] == 1)
+        {
+            int stealTile = 14 - tileIndex;
+            _data[currentPlayerScoreTile] += _data[stealTile];
+            _data[stealTile] = 0;
+        }
+
+        if (HasPlayerValidMove(1 - _currentPlayer))
+        {
+            _currentPlayer = 1 - _currentPlayer;
+        }
+        return true;
     }
 
     public bool IsGameOver()
     {
-
+        return !HasPlayerValidMove(_currentPlayer) && !HasPlayerValidMove(1 - _currentPlayer);
     }
 
     public int GetWinner()
     {
-        
+        if (_data[7] == _data[15])
+        {
+            return 2;
+        }
+        return _data[7] > _data[15] ? 0 : 1;
     }
 
-    public bool IsValidMove()
+    public bool IsValidMove(int index)
     {
-
+        if (index < 0 || index > 7)
+        {
+            return false;
+        }
+        if (_data[index + _currentPlayer * 8] == 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     private bool IsField(int index)
     {
-
+        return index != 7 && index != 15;
     }
 
     private int GetPlayerScoreTitle(int player)
     {
-
+        return 7 + player * 8;
     }
 
     private bool HasPlayerValidMove(int player)
     {
-
+        for (int i = 0; i < 7; i++)
+        {
+            if (_data[i + player * 8] > 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public override string ToString()
